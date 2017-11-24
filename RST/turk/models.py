cdbc1a17dc2cdc86ddecedede591a3326f5e6795
notrodta <rody101@gmail.com>
2017-11-23 19:06:05 -0500

    Need to change profile id to user id in url

--- RST/turk/models.py
+++ RST/turk/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 
 class Profile(models.Model):
+    user = models.ForeignKey(User, default=1)
     name = models.CharField(max_length=250)
     age = models.IntegerField()
     gender = models.BinaryField()
